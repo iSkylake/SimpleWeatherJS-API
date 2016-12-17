5ba3191d0a900cbac765207ccaa8d3e065e733c0
Llorch <SkylakeBeta@gmail.com>
2016-12-17 15:34:12 -0800

    Add city search input

--- public/app.jsx
+++ public/app.jsx
@@ -1,9 +1,48 @@
-var Title = React.createClass({
+var App = React.createClass({
+	getDefaultProps: function(){
+		return{
+			city:""
+		};
+	},
+
+	getInitialState: function(){
+		return{
+			city: this.props.city
+		}
+	},
+
+	onButtonClick: function(e){
+		e.preventDefault();
+
+		var cityRef = this.refs.city;
+
+		var city = cityRef.value;
+		this.refs.city.value = "";
+
+		if(city.length > 0){
+			this.setState({
+				city: city
+			});
+		}
+	},
+
 	render:function(){
+		var city = this.state.city;
 		return(
-			<h1>Weather App</h1>
+			<div>
+				<h1>Weather App</h1>
+				<p>This is an app using Simple Weather JS</p>
+				<p>{city}</p>
+				<form onSubmit={this.onButtonClick}>
+					<input type="text" ref="city" placeholder="City"/>
+					<button>Search</button>
+				</form>
+			</div>
 		);
 	}
 });
 
-ReactDOM.render(<Title/>, document.getElementById('app'));
+ReactDOM.render(
+	<App/>, 
+	document.getElementById('app')
+);
