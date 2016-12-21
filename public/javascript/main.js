9e53c882d7ccd5849273116c4795c3367f884333
Llorch <SkylakeBeta@gmail.com>
2016-12-20 16:34:22 -0800

    Comment out React and add main.js for API functionality

---
+++ public/javascript/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$.simpleWeather({
+		location: 'San Francisco, CA',
+		woeid: '',
+		unit: 'f',
+		success: function(weather){
+			html = '<p>' + weather.temp + '&deg;' + weather.units.temp + '</p>';
+			$("#weather").html(html);
+		},
+		error: function(error){
+			$('#weather').html('<p>' + error + '</p>');
+		}
+	});
+});
