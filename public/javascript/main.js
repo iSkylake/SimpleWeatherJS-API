32a47103367447e6067f2291054dad79aab4d93d
Llorch <SkylakeBeta@gmail.com>
2016-12-21 15:21:54 -0800

    Add search functionality

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
-	$.simpleWeather({
-		location: 'San Francisco, CA',
-		woeid: '',
-		unit: 'f',
-		success: function(weather){
-			html = '<p>' + weather.temp + '&deg;' + weather.units.temp + '</p>';
-			$("#weather").html(html);
-		},
-		error: function(error){
-			$('#weather').html('<p>' + error + '</p>');
-		}
-	});
+
 });
