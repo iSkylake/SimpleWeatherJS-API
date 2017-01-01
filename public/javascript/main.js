4a8301489c0962b2088f216eb98dd8bddd7a2475
Llorch <SkylakeBeta@gmail.com>
2016-12-31 23:03:36 -0800

    Apply style to forecast

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
 				// html = '<p>' + weather.temp + '&deg;' + weather.units.temp + '</p>';
 				// $("#weather").html(html);
 
-				html = '<div class="weather"><div><h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2></div>'+'<div><img class="weather-icon" src="img/weathericons/'+weather.code+'.svg"></img></div></div>';
+				html = '<div class="weather"><div class="temperature"><p>'+weather.temp+'&deg;'+weather.units.temp+'</p></div>'+'<div><img class="weather-icon" src="img/weathericons/'+weather.code+'.svg"></img></div></div>';
 
 				for(var i=0; i<weather.forecast.length; i++){
 
 					// html += '<p>' + weather.forecast[i].day + ':' + weather.forecast[i].high + '</p>';
 					// htmt += '<p><i class="icon-' + weather.code +'"></i>';
-					html += '<div><div><h2>'+weather.forecast[i].high+'&deg;'+weather.units.temp+'</h2></div>'+'<div><img class="weather-icon" src="img/weathericons/'+weather.forecast[i].code+'.svg"></img></div></div>';
+					html += '<div class="weather"><div class="temperature"><p>'+weather.forecast[i].high+'&deg;'+weather.units.temp+'</p></div>'+'<div><img class="weather-icon" src="img/weathericons/'+weather.forecast[i].code+'.svg"></img></div></div>';
 					// html = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
 				}
 				$('#weather').html(html);
