1e3093a0ede398bae4150d06c79b3d52b997ccc5
Llorch <SkylakeBeta@gmail.com>
2016-12-31 23:46:18 -0800

    Clean main.js

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -27,31 +27,3 @@ $(document).ready(function(){
 		});
 	});
 });
-
-	// function searchFunc(){
-	// 	var location = document.getElementById("search").value;
-	// 	console.log(location);
-	// 	$.simpleWeather({
-	// 		location: location,
-	// 		woeid: '',
-	// 		unit: 'f',
-	// 		success: function(weather){
-	// 			// html = '<p>' + weather.temp + '&deg;' + weather.units.temp + '</p>';
-	// 			// $("#weather").html(html);
-
-	// 			html = '<h2><img class="weather-icon" src="img/weathericons/'+weather.code+'.svg"></img> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-
-	// 			for(var i=0; i<weather.forecast.length; i++){
-
-	// 				// html += '<p>' + weather.forecast[i].day + ':' + weather.forecast[i].high + '</p>';
-	// 				// htmt += '<p><i class="icon-' + weather.code +'"></i>';
-	// 				html += '<h2><img class="weather-icon" src="img/weathericons/'+weather.forecast[i].code+'.svg"></img> '+weather.forecast[i].high+'&deg;'+weather.units.temp+'</h2>';
-	// 				// html = '<img class="weathericon" src="images/weathericons/'+weather.code+'.svg">';
-	// 			}
-	// 			$('#weather').html(html);
-	// 		},
-	// 		error: function(error){
-	// 			$('#weather').html('<p>' + error + '</p>');
-	// 		}
-	// 	});
-	// }
